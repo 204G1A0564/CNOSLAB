74196cc250ef402587cf80a836de331ddf1f6d1e
204G1A0564 <94514515+204G1A0564@users.noreply.github.com>
2022-08-30 11:24:13 +0530

    P.Nandini Created Sample.

---
+++ OSLAB/sample.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: P.Nandini
+   Roll No: 204G1A0564
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
