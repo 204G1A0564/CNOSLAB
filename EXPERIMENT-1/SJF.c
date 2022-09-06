dac9034764de53f3924ba0d29d1f50352fdb2c15
204G1A0564 <94514515+204G1A0564@users.noreply.github.com>
2022-09-06 12:31:11 +0530

    Nandini.P created SJF.c

---
+++ EXPERIMENT-1/SJF.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate:P.Nandini
+   Roll No: 204G1A0564
+   Title of the Experiment: CPU Scheduling Using SJF Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
