a308c712468dc7ac40ff09d731c3d66d7a993c8c
204G1A0564 <94514515+204G1A0564@users.noreply.github.com>
2022-10-11 12:44:18 +0530

    P Nandini Created PP.c

---
+++ EXPERIMENT-3/PP.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: P.Nandini
+   Roll No: 204G1A0564
+   Title of the Experiment: A program to stimulate Dining-philosophers problem.
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+*/
