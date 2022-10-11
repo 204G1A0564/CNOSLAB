301f9ddc0c136c4cdc7ff30657b63ce8ec61cf8b
204G1A0564 <94514515+204G1A0564@users.noreply.github.com>
2022-10-11 12:28:14 +0530

    Delete EXPERIMENT-2 directory

--- EXPERIMENT-2/PC.c
+++
@@ -1,6 +0,0 @@
-/* Name Of the Candidate: P.Nandini
-   Roll No: 204G1A0564
-   Title of the Experiment: To simulate producer-consumer problem using semaphores.
-   Date of Creation: 11-10-2022
-   Date of Execution: 11-10-2022
-*/
