c815b638f33a30aedf653ecd20d8fdf41a1270d9
204G1A0544 <204G1A0544@srit.ac.in>
2022-11-10 16:22:13 +0530

    Successfully completed MM.c with outputs

---
+++ EXPERIMENT-4/MVT.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+
+{
+int ms,mp[10],i,
+temp,n=0; char ch = 'y';
+clrscr();
+printf("\nEnter the total memory available (in Bytes)-- ");
+scanf("%d",&ms);
+temp=ms;
+for(i=0;ch=='y';i++,n++)
+{
+printf("\nEnter memory required for process %d (in Bytes) -- ",i+1);
+scanf("%d",&mp[i]);
+if(mp[i]<=temp)
+{
+printf("\nMemory is allocated for Process %d ",i+1);
+temp = temp - mp[i];
+}
+else
+{
+printf("\nMemory is Full"); break;
+}
+printf("\nDo you want to continue(y/n) -- ");
+scanf(" %c", &ch);
+}
+printf("\n\nTotal Memory Available -- %d", ms);
+printf("\n\n\tPROCESS\t\t MEMORY ALLOCATED ");
+for(i=0;i<n;i++)
+printf("\n \t%d\t\t%d",i+1,mp[i]);
+printf("\n\nTotal Memory Allocated is %d",ms-temp);
+printf("\nTotal External Fragmentation is %d",temp);
+getch();
+}/* Name Of the Candidate: P.Nandini
+   Roll No: 204G1A0564
+   Title of the Experiment: MEMORY VARIABLE PARTIONING TYPE (MVT)
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+*/
